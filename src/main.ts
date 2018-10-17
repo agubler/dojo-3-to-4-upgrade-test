21c6a9fb9c716ee41558910c621571198060b481
Anthony Gubler <agubler@sitepen.com>
2018-10-17 18:15:19 +0100

    before upgrade

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
 import { ProjectorMixin } from '@dojo/framework/widget-core/mixins/Projector';
 import HelloWorld from './widgets/HelloWorld';
+import HelloWorldOne from './widgets/HelloWorldOne';
+import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
+import { v, w } from '@dojo/framework/widget-core/d';
 
 import request from '@dojo/framework/core/request';
+import { deepAssign } from '@dojo/framework/core/lang';
+
+deepAssign({}, {});
 
 console.log(request);
+
+class App extends WidgetBase {
+	render() {
+		return v('div', [
+			w(HelloWorld, {}),
+			w(HelloWorldOne, {})
+		]);
+	}
+}
 
 // Create a projector to convert the virtual DOM produced by the application into the rendered page.
 // For more information on starting up a Dojo 2 application, take a look at
 // https://dojo.io/tutorials/002_creating_an_application/
-const Projector = ProjectorMixin(HelloWorld);
+const Projector = ProjectorMixin(App);
 const projector = new Projector();
 
 // By default, append() will attach the rendered content to document.body.  To insert this application
